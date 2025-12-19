a88d15f9cba55a932cd1d7aac451479596d6f0ba
bin278 <1685185241@qq.com>
2025-12-19 22:37:28 +0800

    Initial commit: Generated by mornFront

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200 ease-in-out">
+      Click Me
+    </button>
+  );
+}
+
+export default Button;
